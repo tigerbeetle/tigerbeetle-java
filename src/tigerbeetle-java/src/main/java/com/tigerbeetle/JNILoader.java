134595793e5861e9f674726927a181c0f5b2cb9c
Ian Morgan <ian.j.morgan@gmail.com>
2022-11-14 16:41:23 +0000

    extra test needed for Catalina (OSX 10.15.7)

--- src/tigerbeetle-java/src/main/java/com/tigerbeetle/JNILoader.java
+++ src/tigerbeetle-java/src/main/java/com/tigerbeetle/JNILoader.java
@@ -19,7 +19,7 @@ final class JNILoader {
             if (osName.startsWith("win")) {
                 return OS.win;
             } else if (osName.startsWith("macos") || osName.startsWith("osx")
-                    || osName.startsWith("darwin")) {
+                    || osName.startsWith("darwin") || osName.equals("mac os x")) {
                 return OS.macos;
             } else if (osName.startsWith("linux")) {
                 return OS.linux;
